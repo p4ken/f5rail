96397721e2aca82beb2439baaaeb228edd2d3198
p4ken <commit@p4ken.skr.jp>
2022-01-30 16:49:30 +0900

    rust hello world

--- bin/DRAW.bat
+++ bin/DRAW.bat
@@ -1,4 +1,4 @@
-@REM 【サイン半波長逓減】の曲線を作図
+@REM 緩和曲線を作図する
 @echo off
 REM #jww
 REM #cd
@@ -7,4 +7,4 @@ REM #c  終了R [m] ※無指定は直線 : /_/R2:
 REM #c  TCL [m] : /_/TCL:
 REM #c  間隔 [m] ※無指定は0.1 : /_/DX:
 REM #e
-draw.exe
+draw.exe /_/FUNC:SIN %1 %2 %3 %4 /IO:JWC_TEMP.TXT
