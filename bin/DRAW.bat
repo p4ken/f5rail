3e752ce1f6e0d160c6cab052c1275c02370fd86d
p4ken <commit@p4ken.skr.jp>
2022-01-30 11:42:47 +0900

    bat作成

---
+++ bin/DRAW.bat
@@ -0,0 +1,10 @@
+@REM 【サイン半波長逓減】の曲線を作図
+@echo off
+REM #jww
+REM #cd
+REM #c  開始R [m] ※無指定は直線 : /_/R1:
+REM #c  終了R [m] ※無指定は直線 : /_/R2:
+REM #c  TCL [m] : /_/TCL:
+REM #c  間隔 [m] ※無指定は0.1 : /_/DX:
+REM #e
+draw.exe
