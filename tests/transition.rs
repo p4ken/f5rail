fb88c071ef5cdce3f0cc7594b264494f9c0bc0c7
p4ken <commit@p4ken.skr.jp>
2022-04-03 12:57:03 +0900

    ファイル出力のテストケースを追加

--- tests/transition.rs
+++ tests/transition.rs
@@ -1,51 +1,65 @@
+use std::io::Result as IoResult;
 use std::{
-    ffi::OsString,
+    ffi::{OsStr, OsString},
     fs::File,
     io::{BufRead, BufReader},
+    iter,
 };
 
-use anyhow::{Result};
+use anyhow::Result;
+use derive_more::IntoIterator;
 use encoding_rs::SHIFT_JIS;
 use encoding_rs_io::DecodeReaderBytesBuilder;
 
 use rstest::rstest;
-use tempfile::{NamedTempFile, TempPath};
+use tempfile::NamedTempFile;
 
 #[rstest]
-#[case (vec!["heTCLを指定してください"])]
-fn transition(#[case] expected: Vec<&str>) -> Result<()> {
+#[case(vec!["/TRANSITION:1"], vec!["heTCLを指定してください"])]
+#[case(vec!["/TRANSITION:1", "/R1:-123.4", "/TCL:1"],
+       vec!["h#サイン半波長逓減曲線を描画しました。",
+            "ci 0.00000000000001511214150147834 246.8 246.8 -90 -89.76784530181085"])]
+fn transition(#[case] strv: Vec<&str>, #[case] expected: Vec<&str>) -> Result<()> {
     let path = NamedTempFile::new_in("./tests")?.into_temp_path();
-    let args = vec![Args::file(&path), Args::transition(1)];
-    f5rail::layout(args)?;
+    f5rail::layout(Args::new(&path, &strv))?;
 
     let file = File::open(&path)?;
     let reader = DecodeReaderBytesBuilder::new()
         .encoding(Some(SHIFT_JIS))
         .build(file);
     let reader = BufReader::new(reader);
-    let count = reader
-        .lines()
+    let lines = reader.lines().into_iter().collect::<IoResult<Vec<_>>>()?;
+    let lines = lines
+        .iter()
+        .map(AsRef::as_ref)
+        .map(Option::Some)
+        .chain(iter::once(None));
+    let expects = expected
+        .into_iter()
+        .map(Option::Some)
+        .chain(iter::once(None));
+    lines
+        .zip(expects)
         .enumerate()
-        .inspect(|(i, line)| {
-            let line = line.as_ref().unwrap();
-            assert_eq!(expected.get(*i), Some(&line.as_str()), "line {}", i + 1)
-        })
-        .count();
-    assert_eq!(expected.get(count), None, "line {}", count + 1);
+        .for_each(|(i, (line, expect))| assert_eq!(line, expect, "line {}", i + 1));
 
     path.close()?;
     Ok(())
 }
 
-struct Args;
+#[derive(IntoIterator)]
+struct Args(Vec<OsString>);
 
 impl Args {
-    fn file(path: &TempPath) -> OsString {
-        let mut arg = OsString::from("/FILE:");
-        arg.push(path);
-        arg
-    }
-    fn transition(value: i32) -> OsString {
-        OsString::from(format!("/TRANSITION:{}", value))
+    fn new(path: &impl AsRef<OsStr>, strv: &Vec<impl AsRef<OsStr>>) -> Self {
+        let mut arg_file = OsString::from("/FILE:");
+        arg_file.push(path);
+        let arg_file = arg_file;
+        let args = strv
+            .into_iter()
+            .map(|s| s.into())
+            .chain(iter::once(arg_file))
+            .collect();
+        Self(args)
     }
 }
