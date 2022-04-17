d6604eff105b73e53615a03effb24d786b750b7d
p4ken <commit@p4ken.skr.jp>
2022-04-17 21:28:20 +0900

    範囲指定を2回

--- bat/TRACK-X.bat
+++ bat/TRACK-X.bat
@@ -1,17 +1,16 @@
-@REM BVEマップの他軌道構文を出力します
+
 @echo off
 REM #jww
 REM #cd
-REM #hm | モックアップ |
-REM #hc v(VERSION)
-REM #:1
-REM #1-%d 出力範囲の始点を指定
-REM #2%d 出力範囲の終点を指定
-REM #3%d 出力軌道の通過点を指定
-REM #4%d 自軌道の通過点を指定
-REM #c  出力軌道名 trackkey/_/trackKey:
-REM #c  始点距離程 distance (m)/_/distance:
-REM #c  出力間隔 interval (m)/_/interval:
-REM #c  出力ファイル名 out/_/out:
 REM #hf
+REM #h1
+REM #hc 出力軌道を指定
+REM #c  BVEのトラック名 /_/trackKey:
+REM #1  出力始点を指定
+REM #c  出力始点のBVE距離程 (m)/_/distance:
+REM #c  出力ファイル名 /_/outPath:
 REM #e
+
+REM 出力間隔はここで指定
+
+@REM echo heエラー > JWC_TEMP.txt
---
+++ bat/TRACK.bat
@@ -0,0 +1,12 @@
+@REM BVEマップに出力します
+@echo off
+REM #jww
+REM #cd
+REM #hm | ｘ座標出力 |
+REM #hc v(VERSION)
+REM #:1
+REM #h1
+REM #hc 自軌道を指定
+REM #:2
+REM #h/TRACK-X.bat
+REM #e
