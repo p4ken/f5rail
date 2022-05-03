f6ac745142783767b62c3c9964dc43b255edb480
p4ken <commit@p4ken.skr.jp>
2022-05-03 09:00:42 +0900

    簡略化・スキップボタンを追加

--- bat/TRACK.bat
+++ bat/TRACK.bat
@@ -2,13 +2,25 @@
 @echo off
 REM #jww
 REM #cd
-REM #hm | 自軌道指定 |
+REM #hm | 自軌道指定 | スキップ |
 REM #hc v(VERSION)
 REM #:1
 REM #h1
-REM #hc 自軌道
+REM #hc 自軌道を指定
+REM #1  出力始点を指定
 REM #g1
-REM #h/TRACK-X.bat
+REM #:2
+REM #h/sub/TRACK-N.bat
 REM #e
 
+goto %1
+
+:1
 MOVE JWC_TEMP.txt JWC_TEMP_0.txt
+goto END
+
+:2
+goto END
+
+:END
+echo heエラー > JWC_TEMP.txt
---
+++ bat/sub/TRACK-N.bat
@@ -0,0 +1,17 @@
+
+@echo off
+REM #jww
+REM #cd
+REM #hm | 他軌道指定 | スキップ |
+REM #hf
+REM #:1
+REM #h1
+REM #hc 他軌道を指定
+REM #g1
+REM #c  BVEのトラック名 /_/trackKey:
+REM #:2
+REM #e
+
+REM TODO:出力間隔はここで指定
+
+echo heエラー2 > JWC_TEMP.txt
