4f5551d9045e20ba3ad8d84c71d2d102435fa5e4
p4ken <commit@p4ken.skr.jp>
2022-11-20 17:46:14 +0900

    原点のズレを無くすbatを作成

---
+++ bat/EXPORT.bat
@@ -0,0 +1,17 @@
+@REM 【未実装】BVEの他線座標を計算します
+@echo off
+REM #jww
+REM #hf
+REM #cd
+REM #h4
+REM #0自軌道上の距離基準点を右クリック
+REM #c基準点のＢＶＥ距離程 [メートル]/_/z0:
+REM #1他軌道上の出力「起点」を右クリック
+REM #2他軌道上の出力「終点」を右クリック
+REM #c他軌道のＢＶＥトラック名 /_/track:
+REM #g1
+REM #c出力ファイル名 /_/file:
+REM #e
+
+ECHO %* > log.txt
+ECHO h#map.txtに出力しました > JWC_TEMP.txt
--- bat/TRACK.bat
+++
@@ -1,8 +0,0 @@
-@REM 【未実装】BVEの他線座標を計算します
-@echo off
-REM #jww
-REM #cd
-REM #h/sub/TRACK-0.bat
-REM #e
-
-echo h#v(VERSION) > JWC_TEMP.txt
