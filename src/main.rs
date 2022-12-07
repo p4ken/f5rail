5935c3474e820e08863ea7c8b4b31ac9b668d6e2
p4ken <commit@p4ken.skr.jp>
2022-12-07 21:18:32 +0900

    encoded試用

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::BufRead;
+use std::io::{BufRead, Write};
 
 fn main() -> anyhow::Result<()> {
     println!();
@@ -8,14 +8,16 @@ fn main() -> anyhow::Result<()> {
         println!("{}", arg.to_str().expect("UTF-8への変換に失敗"));
     }
 
+    let sjis = encoded::shift_jis!("日本語");
+    dbg!(sjis);
+
     // let jwc_temp = std::env::var_os("JWC_TEMP").expect("環境変数がありません");
     // println!("{}", jwc_temp.to_string_lossy());
 
+    // 日本語を含むファイルをio::BufReader::linesで読んだら大丈夫か？
     // for line in std::io::BufReader::new(std::fs::File::open("JWC_TEMP.txt")?).lines() {
     //     print!("{}\n", line.unwrap_or("行の読み込みに失敗".to_string()))
     // }
-
-    // 日本語を含むファイルをio::BufReader::linesで読んだら大丈夫か？
 
     // rfd::FileDialog::new()
     //     .add_filter("BVEマップファイル", &["txt"])
